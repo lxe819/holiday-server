a11c2d5478ebbdcba8b5e34c83124885d3fa557e
lxe819 <abeta8352@gmail.com>
2022-09-12 10:28:11 +0800

    Installed morgan

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 require("dotenv").config();
 const express = require("express");
+const morgan = require("morgan");
 
 const PORT = process.env.PORT ?? 3000;
 const app = express();
+
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   res.send({ msg: "Holidays" });
 });
 
 app.listen(PORT, () => {
-  console.log(`Express listing on ${PORT}`);
+  log(`Express listing on ${PORT}`);
 });
